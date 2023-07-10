ba87eb927d71b92fbbca87644943e483b4112fb7
jb1756 <juliusbags04@yahoo.com>
2023-07-10 16:05:06 -0400

    made a function for the menue

--- app.js
+++ app.js
@@ -19,9 +19,54 @@ connection.connect((err) => {
 });
 
 // Function to start your application logic
-function startApp() {
-  // TODO: Implement your application logic here
-}
+function Mainmenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end(); // Close the database connection
+            return;
+        }
+      });
+  }
 
 // Call the function to start your application
 startApp();
